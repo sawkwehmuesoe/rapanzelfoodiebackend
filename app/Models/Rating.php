e9d3b2df0b9b586456feb5c763ed480d89907770
Samuel <sawkwehmuesoe@gmail.com>
2023-12-28 13:17:16 +0630

    add model and table

---
+++ app/Models/Rating.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Rating extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+     
+         'rating',
+         'user_id',
+         'repice_id',
+    
+
+
+    ];
+}
